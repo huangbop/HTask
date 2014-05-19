d7be92322852e5f888caaffb30ff56881db4f0c4
Huang Bo <huangbop@gmail.com>
2014-05-19 16:46:45 +0800

    XXX: clkdivn = 0x5 means 1:4:8

--- startup.c
+++ startup.c
@@ -15,19 +15,19 @@ struct clock_power {
 
 void clock_init(void)
 {
-	/* struct clock_power *const clk_pow = (struct clock_power *)0x4c000000; */
+	struct clock_power *const clk_pow = (struct clock_power *)0x4c000000;
 
-	/* clk_pow->locktime = 0xffffffff; */
+	clk_pow->locktime = 0xffffffff;
 	
-	/* /\* 12.0000MHz to 400.00MHz *\/ */
-	/* clk_pow->mpllcon = (0x5c << 12) | (1 << 4) | 1; */
+	/* 12.0000MHz to 400.00MHz */
+	clk_pow->mpllcon = (0x5c << 12) | (1 << 4) | 1;
 	
-	/* /\* FCLK:HCLK:PCLK = 1:2:4 *\/ */
-	/* clk_pow->clkdivn = 0x3; */
-	/* __asm__ __volatile__ ( */
-	/* 	"mrc p15, 0, r0, c1, c0, 0\n" */
-	/* 	"orr r0, r0, #0xc0000000\n" */
-	/* 	"mcr p15, 0, r0, c1, c0, 0"); */
+	/* FCLK:HCLK:PCLK = 1:4:8 */
+	clk_pow->clkdivn = 0x5;
+	__asm__ __volatile__ (
+		"mrc p15, 0, r0, c1, c0, 0\n"
+		"orr r0, r0, #0xc0000000\n"
+		"mcr p15, 0, r0, c1, c0, 0");
 }
 
 void ht_startup(void)
