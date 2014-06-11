2555d8e416892765f74cf287643de85f879ada13
Huang Bo <huangbop@gmail.com>
2014-06-11 15:30:27 +0800

    Prompt, nand flash align to 2048

--- boot_init.c
+++ boot_init.c
@@ -2,7 +2,24 @@
  * Copyright (C) 2014 Huang Bo
  */
 #include "htask.h"
-#include "nand.h"
+
+void clock_init(void)
+{
+	struct clock_power *const clk_pow = get_base_clock_power();
+
+	clk_pow->locktime = 0xffffffff;
+	
+	/* FCLK:HCLK:PCLK = 1:4:8 */
+	clk_pow->clkdivn = 0x5;
+	
+	/* 12.0000MHz to 400.00MHz */
+	clk_pow->mpllcon = (0x5c << 12) | (1 << 4) | 1;
+	
+	__asm__ __volatile__ (
+		"mrc p15, 0, r0, c1, c0, 0\n"
+		"orr r0, r0, #0xc0000000\n"
+		"mcr p15, 0, r0, c1, c0, 0");
+}
 
 void nand_select_chip(void)
 {
@@ -10,6 +27,7 @@ void nand_select_chip(void)
 	struct nand *nand = get_base_nand();
 
 	nand->NFCONT &= ~(1<<1);
+	for (i = 0; i < 10; i++);
 }
 
 void nand_write_cmd(unsigned char cmd)
@@ -63,21 +81,28 @@ void nand_init(void)
 void nand_write_addr(unsigned addr)
 {
 	int i;
+	int col, row;
 	struct nand *nand = get_base_nand();
 	volatile unsigned char *p = (volatile unsigned char *)&nand->NFADDR;
 
-	*p = addr & 0xff;
+	col = addr % 2048;
+	row = addr / 2048;
+
+	*p = col & 0xff;
 	for(i=0; i<10; i++);
 	
-	*p = (addr >> 9) & 0xff;
+	*p = (col >> 8) & 0x0f;
 	for(i=0; i<10; i++);
 	
-	*p = (addr >> 17) & 0xff;
+	*p = row & 0xff;
 	for(i=0; i<10; i++);
-	
-	*p = (addr >> 25) & 0xff;
+
+	*p = (row >> 8) & 0xff;
 	for(i=0; i<10; i++);
-	
+
+	*p = (row >> 16) & 0x03;
+	for(i=0; i<10; i++);
+
 }
 
 unsigned char nand_read_data(void)
@@ -88,6 +113,7 @@ unsigned char nand_read_data(void)
 	return *p;
 }
 
+/* start & size must align to 2048 */
 void nand_read(unsigned char *buf, unsigned long start, int size)
 {
 	int i, j;
@@ -98,9 +124,10 @@ void nand_read(unsigned char *buf, unsigned long start, int size)
 		nand_write_cmd(0);
 
 		nand_write_addr(i);
+		nand_write_cmd(0x30);
 		nand_wait_idle();
 
-		for (j = 0; j < 512; j++, i++) {
+		for (j = 0; j < 2048; j++, i++) {
 			*buf = nand_read_data();
 			buf++;
 		}
@@ -108,3 +135,11 @@ void nand_read(unsigned char *buf, unsigned long start, int size)
 
 	nand_deselect_chip();
 }
+
+void copy2ram(unsigned long start, unsigned char *buf, int size)
+{
+	nand_init();
+
+	nand_read(buf, start, (size + 2047) & ~(2047));
+}
+
--- main.c
+++ main.c
@@ -1,63 +1,44 @@
 #include "htask.h"
 #include "serial.h"
-#include "nand.h"
+#include "lib.h"
 
-/* #include "lib.h" */
+static char line_buffer[128];
 
-/* static char line_buffer[128]; */
+void read_line()
+{
+	char c;
+	char *p;
 
-/* void read_line() */
-/* { */
-/* 	char c; */
-/* 	char *p; */
+	p = line_buffer;
+	ht_printf("HTask> ");
+	for (;;) {
+		c = serial_getc();
+		if (c <= 'z' && c >= 'a') {
+			serial_putc(c);
+			*p++ = c;
+		}
+		if (c == '\r' || c == '\n') {
+			ht_printf("\n");
+			*p = 0;
+			return;
+		}
 
-/* 	p = line_buffer; */
-/* 	ht_printf("HTask> "); */
-/* 	for (;;) { */
-/* 		c = serial_getc(); */
-/* 		if (c <= 'z' && c >= 'a') { */
-/* 			serial_putc(c); */
-/* 			*p++ = c; */
-/* 		} */
-/* 		if (c == '\r' || c == '\n') { */
-/* 			ht_printf("\n"); */
-/* 			*p = 0; */
-/* 			return; */
-/* 		} */
+	}
+}
 
-/* 	} */
-/* } */
+static char buf[8192] = {'a', 'b',};
+
 
 void main_loop(void)
 {
-	char c;
-	int i;
-	char *p;
-	char r;
+	read_line();
+}
+
+void ht_startup(void)
+{
+	serial_init();
 	
-	c = serial_getc();
-	serial_putc(c);
-	if (c == '\r') {
-		serial_puts("\n\n");
-
-		nand_init();
-
-		nand_wait_idle();
-		nand_write_cmd(0);
-		nand_write_addr(200);
-		nand_wait_idle();
-		r = nand_read_data();
-		serial_putc(r + 'A');
-		
-
+	for (;;) {
+		main_loop();
 	}
-	
-	/* ht_printf("HTask\n"); */
-	/* ht_printf("Copyright (C) 2014 Huang Bo\n\n"); */
-
-	/* for (;;) { */
-	/* 	read_line(); */
-	/* 	ht_printf("Now, we will process this / %s /\n", line_buffer); */
-		
-	/* } */
 }
