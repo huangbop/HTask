6f030c01199aaf8fe6e0aaff1fecdca93ee26504
Huang Bo <huangbop@gmail.com>
2014-07-16 11:20:09 +0800

    Led control with a, b, c

--- main.c
+++ main.c
@@ -4,10 +4,18 @@
 
 static char line_buffer[128];
 
+struct led {
+	volatile unsigned con;
+	volatile unsigned dat;
+};
+
+#define led (*(struct led*)0x56000050)
+
 void read_line()
 {
 	char c;
 	char *p;
+	int i;
 
 	p = line_buffer;
 	ht_printf("HTask> ");
@@ -16,6 +24,12 @@ void read_line()
 		if (c <= 'z' && c >= 'a') {
 			serial_putc(c);
 			*p++ = c;
+			
+			i = c - 'a' + 4; /* a, b, c map to GPF4, 5, 6 */
+			if (i <= 6) {
+				led.dat |= 0xff;
+				led.dat &= ~(1<<i);
+			}
 		}
 		if (c == '\r' || c == '\n') {
 			ht_printf("\n");
@@ -37,6 +51,9 @@ void main_loop(void)
 void ht_startup(void)
 {
 	serial_init();
+
+	led.con = 0x00001500; 	/* 0001 0101 GPF4,5,6 */
+
 	
 	for (;;) {
 		main_loop();
