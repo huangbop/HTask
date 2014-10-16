e4c5be1df5873409b81a57649d8f3d0650013bcd
Huang Bo <huangbop@gmail.com>
2014-10-16 10:40:55 +0800

    Mutex test

--- board/jz2440/application.c
+++ board/jz2440/application.c
@@ -14,58 +14,68 @@ unsigned char thread_b_stack[1024];
 unsigned char thread_c_stack[1024];
 unsigned char thread_d_stack[1024];
 
+struct rt_mutex the_mutex;
+
 void thread_a_entry(void *param)
 {
 	while (1) {
+		rt_mutex_take(&the_mutex, RT_WAITING_FOREVER);
 		rt_kprintf("It's aaa\n");
-		rt_thread_delay(1);
+		rt_mutex_release(&the_mutex);
 	}
 }
 
 void thread_b_entry(void *param)
 {
 	while (1) {
+		rt_mutex_take(&the_mutex, RT_WAITING_FOREVER);
 		rt_kprintf("It's bbb\n");
-		
+		rt_mutex_release(&the_mutex);
 	}
 }
 
 void thread_c_entry(void *param)
 {
 	while (1) {
+		rt_mutex_take(&the_mutex, RT_WAITING_FOREVER);
 		rt_kprintf("It's ccc\n");
-		rt_thread_yield();
+		rt_mutex_release(&the_mutex);
+
 	}
 }
 
 void thread_d_entry(void *param)
 {
 	while (1) {
+		rt_mutex_take(&the_mutex, RT_WAITING_FOREVER);
 		rt_kprintf("It's ddd\n");
-		rt_thread_yield();
+		rt_mutex_release(&the_mutex);
+
 	}
 }
 
 void rt_application_init(void)
 {
 	rt_err_t res;
+
+	rt_mutex_init(&the_mutex, "the mutex", RT_IPC_FLAG_FIFO);
 	
 	res = rt_thread_init(&thread_a, "aaa", thread_a_entry, RT_NULL,
-			     &thread_a_stack[0], 1024, 8, 200);
+			     &thread_a_stack[0], 1024, 10, 3);
 
 	res = rt_thread_init(&thread_b, "bbb", thread_b_entry, RT_NULL,
-			     &thread_b_stack[0], 1024, 9, 200);
+			     &thread_b_stack[0], 1024, 10, 3);
 
 	res = rt_thread_init(&thread_c, "ccc", thread_c_entry, RT_NULL,
-			     &thread_c_stack[0], 1024, 10, 4);
+			     &thread_c_stack[0], 1024, 10, 3);
 
-	res = rt_thread_init(&thread_d, "ddd", thread_d_entry, RT_NULL,
-			     &thread_d_stack[0], 1024, 10, 4);
+	/* res = rt_thread_init(&thread_d, "ddd", thread_d_entry, RT_NULL, */
+	/* 		     &thread_d_stack[0], 1024, 10, 3); */
 	
 
 	rt_thread_startup(&thread_a);
 	rt_thread_startup(&thread_b);
 	rt_thread_startup(&thread_c);
-	rt_thread_startup(&thread_d);
+	/* rt_thread_startup(&thread_d); */
 }
 
