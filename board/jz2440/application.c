d47bbc9c1c63436ac143ab9bf4e29d2836a0a976
Huang Bo <huangbop@gmail.com>
2014-10-20 10:37:10 +0800

    Mailbox test

--- board/jz2440/application.c
+++ board/jz2440/application.c
@@ -14,44 +14,38 @@ unsigned char thread_b_stack[1024];
 unsigned char thread_c_stack[1024];
 unsigned char thread_d_stack[1024];
 
-struct rt_event the_event;
+struct rt_mailbox the_mb;
+char mb_buf[40];
 
 void thread_a_entry(void *param)
 {
-	static int flag = 0;
+	unsigned c = 'x';
 
 	while (1) {
-		flag = !flag;
-		rt_kprintf("It's aaa, %d \n", flag);
-
-		if (flag)
-			rt_event_send(&the_event, (1 << 3));
-		else 
-			rt_event_send(&the_event, (1 << 7));
+		rt_mb_send_wait(&the_mb, c, RT_WAITING_FOREVER);
+		rt_kprintf("It's aaa\n");
+		
 	}
 }
 
 void thread_b_entry(void *param)
 {
-	unsigned recv;
+	unsigned c;
 
 	while (1) {
-		if (rt_event_recv(&the_event, 1 << 3, RT_EVENT_FLAG_AND | RT_EVENT_FLAG_CLEAR, 
-				  RT_WAITING_FOREVER, &recv) == RT_EOK) {
-			rt_kprintf("It's bbb, %d\n", recv);
-		}
+		rt_mb_recv(&the_mb, &c, RT_WAITING_FOREVER);
+		rt_kprintf("recv %c from b\n", c);
+		
 	}
 }
 
 void thread_c_entry(void *param)
 {
-	unsigned recv;
-	
+	unsigned c;
+
 	while (1) {
-		if (rt_event_recv(&the_event, 1 << 7, RT_EVENT_FLAG_AND | RT_EVENT_FLAG_CLEAR, 
-			      RT_WAITING_FOREVER, &recv) == RT_EOK) {
-			rt_kprintf("It's ccc, %d\n", recv);			
-		}
+		rt_mb_recv(&the_mb, &c, RT_WAITING_FOREVER);
+		rt_kprintf("recv %c from c\n", c);
 	}
 }
 
@@ -69,16 +63,16 @@ void rt_application_init(void)
 {
 	rt_err_t res;
 
-	rt_event_init(&the_event, "the event", RT_IPC_FLAG_FIFO);
+	rt_mb_init(&the_mb, "the mb", mb_buf, sizeof(mb_buf)/4, RT_IPC_FLAG_FIFO);
 	
 	res = rt_thread_init(&thread_a, "aaa", thread_a_entry, RT_NULL,
-			     &thread_a_stack[0], 1024, 10, 5);
+			     &thread_a_stack[0], 1024, 9, 5);
 
 	res = rt_thread_init(&thread_b, "bbb", thread_b_entry, RT_NULL,
-			     &thread_b_stack[0], 1024, 10, 6);
+			     &thread_b_stack[0], 1024, 10, 5);
 
 	res = rt_thread_init(&thread_c, "ccc", thread_c_entry, RT_NULL,
-			     &thread_c_stack[0], 1024, 10, 7);
+			     &thread_c_stack[0], 1024, 10, 5);
 
 	/* res = rt_thread_init(&thread_d, "ddd", thread_d_entry, RT_NULL, */
 	/* 		     &thread_d_stack[0], 1024, 10, 3); */
